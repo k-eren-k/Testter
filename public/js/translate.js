3667c310ad0bf4947e67caffc06ef2957ae74833
Eren <114621741+k-eren-k@users.noreply.github.com>
2023-12-16 21:14:11 +0300

    Update translate.js

--- public/js/translate.js
+++ public/js/translate.js
@@ -1,63 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
+    let isTurkish = false;
 
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v6.0.0-beta1/css/all.css" />
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-  <link rel="stylesheet" href="/css/main.css" />
-  <link rel="stylesheet" href="/css/bar.css" />
-  <link rel="stylesheet" href="/css/about.css" />
-  <link rel="stylesheet" href="/css/proje.css" />
-  <link rel="stylesheet" href="/css/right-scroll.css" />
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v6.0.0-beta1/css/all.css" />
+    function translateText() {
+        const textContainerText = document.getElementById("textContainer");
+        const translateIcon = document.getElementById("translateIcon");
 
-</head>
+        textContainerText.classList.add("fade-in-out");
+        textContainerText.innerHTML = "Çeviri Yapılıyor Lütfen Bekle <span id='loadingDots'></span>";
+        animateDots();
 
-<body>
-  <div class="bar" id="bar"><%- include('pages/bar.ejs') %></div>
+        setTimeout(() => {
+            if (isTurkish) {
+                textContainerText.innerText = "I'm a Web Developer from Turkey";
+                translateIcon.innerText = "translate";
+            } else {
+                textContainerText.innerText = "Türkiye'den Web Geliştiricisiyim";
+                translateIcon.innerText = "A";
+            }
 
-  <img alt="Picture of the author" fetchpriority="high" loading="eager" width="600" height="600" decoding="async"
-    data-nimg="1" class="flex justify-center items-center select-none fixed z--10 opacity-100"
-    style="color: transparent; max-width: 100%; height: auto" src="https://www.sametcc.me/blobanimation.svg" />
+            textContainerText.classList.remove("fade-in-out");
 
-  <p id="dynamicText">This is a centered paragraph on top of the image.</p>
-  <div class="text-container" id="textContainer">
-    <span class="text-container-span">I'm a Web Developer from Turkey</span>
-    <a id="translateButton" onclick="translateText()">
-      <span class="material-symbols-outlined" id="translateIcon">translate</span>
-    </a>
-  </div>
-  <style>
-    .fade-in-out {
-      transition: opacity 1s;
+            isTurkish = !isTurkish;
+        }, 2200); 
     }
-  </style>
 
+    function animateDots() {
+        const dotsElement = document.getElementById('loadingDots');
+        let dots = 0;
+        setInterval(() => {
+            dots = (dots + 1) % 4;
+            dotsElement.innerHTML = ".".repeat(dots);
+        }, 300); 
+    }
 
-  <div class="button-container">
-    <button><i class="fa-brands fa-discord"></i></button>
-    <button><i class="fa-brands fa-github"></i></button>
-    <button><i class="fa-solid fa-envelope"></i></button>
-  </div>
+  const originalText = document.getElementById('abouttextSpan').textContent;
 
+  function toggleTranslation() {
+    const abouttextSpan = document.getElementById('abouttextSpan');
+    if (isTurkish) {
+      abouttextSpan.textContent = originalText;
+    } else {
+      abouttextSpan.textContent = "Merhaba, Ben Eren. 14 yaşındayım, lise birinci sınıfta okuyorum ve web alanında ilerleyip yazılım mühendisi olmayı hedefliyorum. Node.js'i programlama dil olarak kullanıyorum ve Express'i bir çerçeve olarak kullanıyorum. Ayrıca zaman zaman React kullanıyorum. Daha fazla bilgi için sosyal medya hesaplarıma göz atabilirsiniz.";
+    }
 
-  <div class="proje"><%- include('pages/proje.ejs') %></div>
-  <div class="about"><%- include('pages/about.ejs') %></div>
-  <script src="/js/hello.js"></script>
-  <script src="/js/date.js"></script>
-  <script src="/js/scroll.js"></script>
-  <script src="/js/bar.js"></script>
-  <script src="/js/web.js"></script>
-  <script src="/js/right-scroll.js"></script>
-  <script src="/js/translate.js"></script>
-
-  <script></script>
-</body>
-<div id="navigate-button" class="navigate-button">
-  <i class="fa-light fa-chevron-up"></i>
-</div>
-
-</html>
+    isTurkish = !isTurkish;
+  }
